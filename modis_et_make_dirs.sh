10c9075fa532870fd1e77bc5b36d385751ba541b
jcabaker <40062196+jcabaker@users.noreply.github.com>
2020-11-13 10:15:24 +0000

    Update modis_et_make_dirs.sh

--- modis_et_make_dirs.sh
+++ modis_et_make_dirs.sh
@@ -3,23 +3,23 @@
 # and copy relevant data to the correct directory
 
 for num in {2001..2020} ; do
-        mkdir $num
-        n=1
-        max=365
-        while [ "$n" -le "$max" ]; do
-                if [ $n -lt 10 ]; then
-                        mkdir "$num"/"00$n"
-                        mv MOD16A2.A"$num"00"$n"*.hdf "$num"/"00$n"/
-                else
-                if [ $n -lt 100 ]; then
-                        mkdir "$num"/"0$n"
-                        mv MOD16A2.A"$num"0"$n"*.hdf "$num"/"0$n"/
-                else
-                mkdir "$num"/"$n"
-                        mv MOD16A2.A"$num""$n"*.hdf "$num"/"$n"/
-                fi
-                fi
-        n=`expr "$n" + 8`;
+	mkdir $num
+	n=1
+	max=365
+	while [ "$n" -le "$max" ]; do
+        	if [ $n -lt 10 ]; then
+                	mkdir "$num"/"00$n"
+			mv MOD16A2.A"$num"00"$n"*.hdf "$num"/"00$n"/
+        	else
+        	if [ $n -lt 100 ]; then
+                	mkdir "$num"/"0$n"
+			mv MOD16A2.A"$num"0"$n"*.hdf "$num"/"0$n"/
+        	else
+        	mkdir "$num"/"$n"
+			mv MOD16A2.A"$num""$n"*.hdf "$num"/"$n"/
+        	fi
+		fi
+	n=`expr "$n" + 8`;
 
 
 done

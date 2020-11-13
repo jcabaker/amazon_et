73cc21ea193cf45da0b32db54108a82f63c44de7
jcabaker <40062196+jcabaker@users.noreply.github.com>
2020-11-13 10:01:34 +0000

    Update gleam_et_data_processing.py

--- gleam_et_data_processing.py
+++ gleam_et_data_processing.py
@@ -17,19 +17,18 @@ regrid_cube = one_deg_cube
 data_path = '/nfs/a68/gyjcab/datasets/GLEAM/v3.3/v3.3b/monthly/'
 #cube = iris.load_cube(data_path + 'E_1980_2018_GLEAM_v3.3a_MO.nc')
 cube = iris.load_cube(data_path + 'E_2003_2018_GLEAM_v3.3b_MO.nc')
-print(cube)
-assert False
+
+# reorder coordinates
 cube.transpose([0, 2, 1])
 print(cube)
 cube.standard_name = 'water_evaporation_flux'
 name = 'water_evaporation_flux'
 cube.convert_units('mm.s-1')
 cube.units = 'kg m^-2 s^-1'
-print(cube)
 
 # call harmonise.cube2netcdf function to regrid and save as new cube
 harmonise.cube2netcdf(cube, 'evspsbl', startyr=2003, nyear=16,
-                      product='gleam_3.3b', regrid=True, latlon_bounds=True,
+                      product='gleam_3.3b_test', regrid=True, latlon_bounds=True,
                       regrid_cube=regrid_cube, time_bounds=False, name=name)
 
    
